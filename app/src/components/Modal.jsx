5b97a9b2a03d283e469c4829d3f2f5169b4bbfae
benmneb <62878664+benmneb@users.noreply.github.com>
2021-08-17 23:30:09 +0800

    initialise modal

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,3 +1,42 @@
-export default function Modal() {
-	return <section>this is the modal</section>;
+import { forwardRef, useState } from 'react';
+
+import { useHistory, useParams } from 'react-router-dom';
+
+import { Dialog, Fade } from '@material-ui/core';
+
+import { Recipe } from './index';
+
+const Transition = forwardRef((props, ref) => {
+	const history = useHistory();
+
+	return (
+		<Fade
+			ref={ref}
+			{...props}
+			onExited={() => {
+				history.goBack();
+			}}
+		/>
+	);
+});
+
+export default function Modal(props) {
+	const [open, setOpen] = useState(props.open);
+
+	const { id } = useParams();
+
+	function handleClose() {
+		setOpen(false);
+	}
+
+	return (
+		<Dialog
+			open={open}
+			onClose={handleClose}
+			TransitionComponent={Transition}
+			scroll="body"
+		>
+			<Recipe id={id} />
+		</Dialog>
+	);
 }
