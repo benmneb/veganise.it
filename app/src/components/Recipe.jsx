3bb0c6c5cbc337808da4a863c7a643ada9cfbc92
benmneb <62878664+benmneb@users.noreply.github.com>
2021-08-17 23:30:58 +0800

    initialise recipe component

--- app/src/components/Recipe.jsx
+++ app/src/components/Recipe.jsx
@@ -1,3 +1,14 @@
+// import { useParams } from 'react-router-dom';
+
+import { DialogTitle, DialogContent } from '@material-ui/core';
+
 export default function Recipe() {
-	return <section>mmm recipe yum</section>;
+	// const { id } = useParams();
+
+	return (
+		<section>
+			<DialogTitle>Delicious</DialogTitle>
+			<DialogContent>yummy recipes</DialogContent>
+		</section>
+	);
 }
