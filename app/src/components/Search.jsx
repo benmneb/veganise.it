792e22749c893e639b6a5d6551c74cebabee3725
benmneb <62878664+benmneb@users.noreply.github.com>
2021-08-17 23:30:43 +0800

    pimp out search design

--- app/src/components/Search.jsx
+++ app/src/components/Search.jsx
@@ -1,41 +1,86 @@
 import { useEffect, useRef, useMemo } from 'react';
-import { Button, styled, FormControl, OutlinedInput } from '@material-ui/core/';
+import {
+	Button,
+	styled,
+	FormControl,
+	OutlinedInput,
+	useScrollTrigger,
+	Grow,
+} from '@material-ui/core/';
 import { useFormControl } from '@material-ui/core/FormControl';
 
 import Typed from 'typed.js';
 
-const Form = styled(FormControl)(({ theme }) => ({
-	width: '100%',
+const FormController = styled(FormControl)(({ theme }) => ({
+	position: 'sticky',
+	top: theme.spacing(4),
+	zIndex: theme.zIndex.appBar,
+	backgroundColor: theme.palette.background.paper,
+	borderRadius: theme.shape.borderRadius,
 	display: 'flex',
 	flexDirection: 'row',
+	[theme.breakpoints.down('tablet')]: {
+		flexDirection: 'column',
+	},
 	justifyContent: 'center',
 	alignItems: 'center',
 	margin: theme.spacing(0, 4, 4),
 }));
 
 const TextField = styled(OutlinedInput)(({ theme }) => ({
+	height: 80,
 	fontSize: theme.typography.h3.fontSize,
 	fontWeight: theme.typography.h3.fontWeight,
 	borderRadius: `${theme.shape.borderRadius}px 0 0 ${theme.shape.borderRadius}px`,
-	height: 80,
+	[theme.breakpoints.down('tablet')]: {
+		width: '100%',
+		fontSize: theme.typography.h4.fontSize,
+		borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
+	},
 	'& .MuiOutlinedInput-input': {
 		textAlign: 'center',
 	},
 }));
 
+const Box = styled('div')(({ theme }) => ({
+	[theme.breakpoints.down('tablet')]: {
+		width: '100%',
+		maxWidth: 458,
+		display: 'flex',
+		justifyContent: 'center',
+	},
+}));
+
 const SearchButton = styled(Button)(({ theme }) => ({
+	height: 80,
 	fontSize: theme.typography.h3.fontSize,
 	fontWeight: theme.typography.h3.fontWeight,
 	borderRadius: `0 ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0`,
-	height: 80,
+	[theme.breakpoints.down('tablet')]: {
+		fontSize: theme.typography.h4.fontSize,
+		borderRadius: `0 0 ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px`,
+		width: '100%',
+	},
 }));
+
+function HideOnScroll(props) {
+	const { children } = props;
+
+	const trigger = useScrollTrigger({ threshold: '520' });
+
+	return (
+		<Grow appear={false} direction="down" in={!trigger}>
+			{children}
+		</Grow>
+	);
+}
 
 function TypedInputs() {
 	const { focused } = useFormControl() || {};
 
-	const input = useRef(null);
-	const string = useRef(null);
-	const typed = useRef(null);
+	const inputRef = useRef(null);
+	const stringRef = useRef(null);
+	const typedRef = useRef(null);
 
 	// initialise Typed.js on load
 	useEffect(() => {
@@ -56,36 +101,36 @@ function TypedInputs() {
 			loop: true,
 			shuffle: true,
 			preStringTyped: (arrayPos, self) =>
-				(string.current = self.strings[self.sequence[arrayPos]]),
+				(stringRef.current = self.strings[self.sequence[arrayPos]]),
 		};
 
-		typed.current = new Typed(input.current, options);
+		typedRef.current = new Typed(inputRef.current, options);
 
 		return () => {
-			typed.current.destroy();
+			typedRef.current.destroy();
 		};
 	}, []);
 
 	const placeholder = useMemo(() => {
 		if (focused) {
-			typed.current.destroy();
+			typedRef.current.destroy();
 			return 'Your favourite meal...';
 		}
 
-		typed.current?.reset();
+		typedRef.current?.reset();
 		return undefined;
 	}, [focused]);
 
 	function handleSearch() {
-		console.log(string.current);
+		console.log(stringRef.current);
 	}
 
 	return (
 		<>
-			<div>
-				<TextField placeholder={placeholder} inputRef={input} />
-			</div>
-			<div>
+			<Box>
+				<TextField placeholder={placeholder} inputRef={inputRef} />
+			</Box>
+			<Box>
 				<SearchButton
 					disableElevation
 					variant="contained"
@@ -93,15 +138,17 @@ function TypedInputs() {
 				>
 					Veganise It!
 				</SearchButton>
-			</div>
+			</Box>
 		</>
 	);
 }
 
 export default function Search() {
 	return (
-		<Form component="form">
-			<TypedInputs />
-		</Form>
+		<HideOnScroll>
+			<FormController component="form">
+				<TypedInputs />
+			</FormController>
+		</HideOnScroll>
 	);
 }
