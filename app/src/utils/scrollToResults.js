4f181c584b6ea918053857e508a77c269da0a671
benmneb <62878664+benmneb@users.noreply.github.com>
2021-10-05 20:12:53 +0800

    fix search-bar-being-in-the-way-after-searching-while-scrolled-down bug on normaly browsers (ie. not safari)

--- app/src/utils/scrollToResults.js
+++ app/src/utils/scrollToResults.js
@@ -4,11 +4,24 @@ smoothscroll.polyfill();
 
 export function scrollToResults() {
 	const element = document.getElementById('caption');
-	const offset = 32;
+	const offset = 37; // + 1 because of `adjustForSearchBar()`
 	const bodyRect = document.body.getBoundingClientRect().top;
 	const elementRect = element.getBoundingClientRect().top;
 	const elementPosition = elementRect - bodyRect;
 	const offsetPosition = elementPosition - offset;
+
+	function adjustForSearchBar() {
+		if (window.scrollY.toFixed() !== offsetPosition.toFixed()) return;
+
+		window.removeEventListener('scroll', adjustForSearchBar);
+		window.scrollBy({
+			top: 1,
+			behavior: 'smooth',
+		});
+	}
+
+	window.addEventListener('scroll', adjustForSearchBar);
+	adjustForSearchBar();
 
 	window.scrollTo({
 		top: offsetPosition,
