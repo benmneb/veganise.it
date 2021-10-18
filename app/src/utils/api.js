66bfed8e51a9a914dd5a6fd84258285194904ec9
benmneb <62878664+benmneb@users.noreply.github.com>
2021-10-18 08:35:31 +0800

    update axios base url for deployment

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: process.env.AXIOS_BASE_URL || 'http://localhost:5000',
+	baseURL:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:5000'
+			: 'https://veganise-it-api.herokuapp.com',
 });
