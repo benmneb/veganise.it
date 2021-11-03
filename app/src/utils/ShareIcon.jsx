ad92ed7320f6a57a6032ca0a74070cfbb7c203a1
benmneb <62878664+benmneb@users.noreply.github.com>
2021-11-03 17:02:32 +0800

    use `navigator.userAgentData` when available

--- app/src/utils/ShareIcon.jsx
+++ app/src/utils/ShareIcon.jsx
@@ -1,7 +1,9 @@
 import { useMediaQuery } from '@mui/material';
 import { ShareRounded, IosShareRounded } from '@mui/icons-material';
 
-const isApple = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent);
+const isApple = navigator.userAgentData
+	? /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgentData.platform)
+	: /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent);
 
 export default function ShareIcon(props) {
 	const { children, ...others } = props;
